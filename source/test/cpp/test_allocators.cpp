4020dda5ac60d29ada2d47ff27999f9f8560a88c
jurgen.kluft <jurgen.kluft@gmail.com>
2023-05-15 08:28:53 +0800

    * unittest; test allocators

---
+++ source/test/cpp/test_allocators.cpp
@@ -0,0 +1,51 @@
+#include "ccore/c_target.h"
+#include "cbenchmark/cbenchmark.h"
+#include "cbenchmark/private/c_benchmark_results.h"
+#include "cbenchmark/private/c_benchmark_state.h"
+#include "cbenchmark/private/c_benchmark_unit.h"
+#include "cbenchmark/private/c_benchmark_reporter.h"
+#include "cbenchmark/private/c_benchmark_allocators.h"
+
+#include "cunittest/cunittest.h"
+
+#include <string>
+
+using namespace ncore;
+
+namespace BenchMark
+{
+    class TestAllocator : public Allocator
+    {
+    public:
+        s32                      num_allocations;
+        UnitTest::TestAllocator* allocator;
+
+        TestAllocator(UnitTest::TestAllocator* a)
+            : num_allocations(0)
+            , allocator(a)
+        {
+        }
+
+        virtual void* v_Allocate(unsigned int size, unsigned int alignment = sizeof(void*));
+        virtual void  v_Deallocate(void* ptr);
+    };
+
+} // namespace BenchMark
+
+UNITTEST_SUITE_BEGIN(test_allocators)
+{
+    UNITTEST_FIXTURE(main)
+    {
+        UNITTEST_FIXTURE_SETUP() {}
+        UNITTEST_FIXTURE_TEARDOWN() {}
+
+        UNITTEST_TEST(test)
+        {
+            BenchMark::TestAllocator ta(TestAllocator);
+
+            BenchMark::ForwardAllocator a;
+            a.Initialize(&ta, 1024);
+        }
+    }
+}
+UNITTEST_SUITE_END
